c8d025c59c629506ee74d3cc3ea512ce6cde369a
Kana <d20035432@gmail.com>
2023-12-30 21:46:23 +0800

    商品功能-實作查詢商品列表（基礎實作）

--- springboot-mall/src/main/java/com/pengchu/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/pengchu/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.pengchu.springbootmall.dao;
 import com.pengchu.springbootmall.dto.ProductRequest;
 import com.pengchu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- springboot-mall/src/main/java/com/pengchu/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/pengchu/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.pengchu.springbootmall.service;
 import com.pengchu.springbootmall.dto.ProductRequest;
 import com.pengchu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
